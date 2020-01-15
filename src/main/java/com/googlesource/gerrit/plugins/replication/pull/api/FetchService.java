01ad2d1dc5ba0a46a0ff4e8bce67dc1f3d7dcd34
Marcin Czech <maczech@gmail.com>
2020-01-15 15:16:38 +0100

    RestApi service for fetch based on sha1
    Pull replication rest api allows to notify service to fetch from remote
    specific ref update by sha1. This will allow to implement pull replication
    functionality.
    
    Feature: Issue 11605
    Change-Id: I81793e6aeb6e5ffc4679ace7b282948d9720f30f

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/api/FetchService.java
@@ -0,0 +1,88 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication.pull.api;
+
+import com.google.gerrit.entities.Project;
+import com.google.inject.Inject;
+import com.googlesource.gerrit.plugins.replication.pull.Command;
+import com.googlesource.gerrit.plugins.replication.pull.FetchResultProcessing;
+import com.googlesource.gerrit.plugins.replication.pull.PullReplicationStateLogger;
+import com.googlesource.gerrit.plugins.replication.pull.ReplicationState;
+import com.googlesource.gerrit.plugins.replication.pull.Source;
+import com.googlesource.gerrit.plugins.replication.pull.SourcesCollection;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import org.eclipse.jgit.transport.URIish;
+
+public class FetchService implements Command {
+
+  private ReplicationState.Factory fetchReplicationStateFactory;
+  private PullReplicationStateLogger fetchStateLog;
+  SourcesCollection sources;
+
+  @Inject
+  public FetchService(
+      ReplicationState.Factory fetchReplicationStateFactory,
+      PullReplicationStateLogger fetchStateLog,
+      SourcesCollection sources) {
+    this.fetchReplicationStateFactory = fetchReplicationStateFactory;
+    this.fetchStateLog = fetchStateLog;
+    this.sources = sources;
+  }
+
+  public void fetch(Project.NameKey name, URIish url, String sha1)
+      throws InterruptedException, ExecutionException {
+    ReplicationState state =
+        fetchReplicationStateFactory.create(new FetchResultProcessing.CommandProcessing(this));
+    Optional<Source> source =
+        sources.getAll().stream().filter(s -> s.getURIs(name, null).contains(url)).findFirst();
+    if (!source.isPresent()) {
+      fetchStateLog.warn("Unknown configuration section for:" + url.getPath(), state);
+      return;
+    }
+    Future<?> future = source.get().schedule(name, sha1, url, state, true);
+    state.markAllFetchTasksScheduled();
+    if (future != null) {
+      try {
+        future.get();
+      } catch (InterruptedException e) {
+        fetchStateLog.error(
+            "Thread was interrupted while waiting for fetch operation to finish", e, state);
+        throw e;
+      } catch (ExecutionException e) {
+        fetchStateLog.error("An exception was thrown in fetch operation", e, state);
+        throw e;
+      }
+    }
+
+    if (state.hasFetchTask()) {
+      try {
+        state.waitForReplication();
+      } catch (InterruptedException e) {
+        writeStdErrSync("We are interrupted while waiting replication to complete");
+        throw e;
+      }
+    } else {
+      writeStdOutSync("Nothing to replicate");
+    }
+  }
+
+  @Override
+  public void writeStdOutSync(String message) {}
+
+  @Override
+  public void writeStdErrSync(String message) {}
+}
