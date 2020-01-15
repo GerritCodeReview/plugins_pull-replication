c0b91d900f9f6b3cf71ebd83d7e59ebe37ec5540
Marcin Czech <maczech@gmail.com>
2020-01-15 15:16:38 +0100

    [DO NOT MERGE] RestApi service for fetch based on sha1
    Pull replication rest api allows to notify service to fetch from remote
    specific ref update by sha1. This will allow to implement pull replication
    functionality.
    
    Feature: Issue 11605
    Change-Id: I81793e6aeb6e5ffc4679ace7b282948d9720f30f

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/api/FetchAction.java
@@ -0,0 +1,52 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication.pull.api;
+
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.server.project.ProjectResource;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.replication.ReplicationFileBasedConfig;
+import com.googlesource.gerrit.plugins.replication.pull.api.FetchAction.Input;
+import org.eclipse.jgit.transport.URIish;
+
+@Singleton
+public class FetchAction implements RestModifyView<ProjectResource, Input> {
+  FetchService service;
+
+  @Inject
+  public FetchAction(FetchService service) {
+    this.service = service;
+  }
+
+  public static class Input {
+    public String url;
+    public String sha1;
+  }
+
+  @Override
+  public Response<?> apply(ProjectResource resource, Input input)
+      throws AuthException, BadRequestException, ResourceConflictException, Exception {
+    service.fetch(
+        resource.getNameKey(),
+        new URIish(ReplicationFileBasedConfig.replaceName(input.url, resource.getName(), false)),
+        input.sha1);
+    return Response.created(input);
+  }
+}
