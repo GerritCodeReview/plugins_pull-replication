c1d7539dbcc0db8c071e83cd8e2edef1e0c22b34
Marcin Czech <maczech@gmail.com>
2020-01-15 15:16:38 +0100

    RestApi service for fetch based on object id
    Pull replication rest api allows to notify service to fetch from remote
    specific ref update by object id. This will allow to implement pull
    replication functionality.
    
    Feature: Issue 11605
    Change-Id: I81793e6aeb6e5ffc4679ace7b282948d9720f30f

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/api/FetchService.java
@@ -0,0 +1,91 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication.pull.api;
+
+import com.google.gerrit.entities.Project;
+import com.google.inject.Inject;
+import com.googlesource.gerrit.plugins.replication.pull.Command;
+import com.googlesource.gerrit.plugins.replication.pull.FetchResultProcessing;
+import com.googlesource.gerrit.plugins.replication.pull.PullReplicationStateLogger;
+import com.googlesource.gerrit.plugins.replication.pull.ReplicationState;
+import com.googlesource.gerrit.plugins.replication.pull.Source;
+import com.googlesource.gerrit.plugins.replication.pull.SourcesCollection;
+import com.googlesource.gerrit.plugins.replication.pull.api.exception.RemoteConfigurationMissingException;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class FetchService implements Command {
+
+  private ReplicationState.Factory fetchReplicationStateFactory;
+  private PullReplicationStateLogger fetchStateLog;
+  SourcesCollection sources;
+
+  @Inject
+  public FetchService(
+      ReplicationState.Factory fetchReplicationStateFactory,
+      PullReplicationStateLogger fetchStateLog,
+      SourcesCollection sources) {
+    this.fetchReplicationStateFactory = fetchReplicationStateFactory;
+    this.fetchStateLog = fetchStateLog;
+    this.sources = sources;
+  }
+
+  public void fetch(Project.NameKey name, String label, String objectId)
+      throws InterruptedException, ExecutionException, RemoteConfigurationMissingException {
+    ReplicationState state =
+        fetchReplicationStateFactory.create(new FetchResultProcessing.CommandProcessing(this));
+    Optional<Source> source =
+        sources.getAll().stream().filter(s -> s.getRemoteConfigName().equals(label)).findFirst();
+    if (!source.isPresent()) {
+      String msg = String.format("Remote configuration section %s not found", label);
+      fetchStateLog.error(msg, state);
+      throw new RemoteConfigurationMissingException(msg);
+    }
+
+    try {
+      Future<?> future = source.get().schedule(name, objectId, state, true);
+      state.markAllFetchTasksScheduled();
+      future.get();
+    } catch (InterruptedException e) {
+      fetchStateLog.error(
+          "Thread was interrupted while waiting for fetch operation to finish", e, state);
+      throw e;
+    } catch (ExecutionException e) {
+      fetchStateLog.error("An exception was thrown in fetch operation", e, state);
+      throw e;
+    } catch (IllegalStateException e) {
+      fetchStateLog.error("Exception during the fetch operation", e, state);
+      throw e;
+    }
+
+    if (state.hasFetchTask()) {
+      try {
+        state.waitForReplication();
+      } catch (InterruptedException e) {
+        writeStdErrSync("We are interrupted while waiting replication to complete");
+        throw e;
+      }
+    } else {
+      writeStdOutSync("Nothing to replicate");
+    }
+  }
+
+  @Override
+  public void writeStdOutSync(String message) {}
+
+  @Override
+  public void writeStdErrSync(String message) {}
+}
---
+++ src/test/java/com/googlesource/gerrit/plugins/replication/pull/api/FetchActionTest.java
@@ -0,0 +1,161 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication.pull.api;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doThrow;
+
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
+import com.google.gerrit.server.project.ProjectResource;
+import com.googlesource.gerrit.plugins.replication.pull.api.exception.RemoteConfigurationMissingException;
+import java.util.concurrent.ExecutionException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FetchActionTest {
+  FetchAction fetchAction;
+  String label = "instance-2-label";
+  String url = "file:///gerrit-host/instance-1/git/${name}.git";
+  String objectId = "c90989ed7a8ab01f1bdd022872428f020b866358";
+
+  @Mock FetchService fetchService;
+  @Mock ProjectResource projectResource;
+
+  @Before
+  public void setup() {
+    fetchAction = new FetchAction(fetchService);
+  }
+
+  @Test
+  public void shouldReturnCreatedResponseCode() throws RestApiException {
+    FetchAction.Input inputParams = new FetchAction.Input();
+    inputParams.label = label;
+    inputParams.object_id = objectId;
+
+    Response<?> response = fetchAction.apply(projectResource, inputParams);
+
+    assertThat(response.statusCode()).isEqualTo(201);
+  }
+
+  @SuppressWarnings("cast")
+  @Test
+  public void shouldReturnSourceUrlAndObjectIdAsAResponseBody() throws Exception {
+    FetchAction.Input inputParams = new FetchAction.Input();
+    inputParams.label = label;
+    inputParams.object_id = objectId;
+
+    Response<?> response = fetchAction.apply(projectResource, inputParams);
+
+    assertThat((FetchAction.Input) response.value()).isEqualTo(inputParams);
+  }
+
+  @Test(expected = BadRequestException.class)
+  public void shouldThrowBadRequestExceptionWhenMissingLabel() throws Exception {
+    FetchAction.Input inputParams = new FetchAction.Input();
+    inputParams.object_id = objectId;
+
+    fetchAction.apply(projectResource, inputParams);
+  }
+
+  @Test(expected = BadRequestException.class)
+  public void shouldThrowBadRequestExceptionWhenEmptyLabel() throws Exception {
+    FetchAction.Input inputParams = new FetchAction.Input();
+    inputParams.label = "";
+    inputParams.object_id = objectId;
+
+    fetchAction.apply(projectResource, inputParams);
+  }
+
+  @Test(expected = BadRequestException.class)
+  public void shouldThrowBadRequestExceptionWhenMissingObjectId() throws Exception {
+    FetchAction.Input inputParams = new FetchAction.Input();
+    inputParams.label = label;
+
+    fetchAction.apply(projectResource, inputParams);
+  }
+
+  @Test(expected = BadRequestException.class)
+  public void shouldThrowBadRequestExceptionWhenEmptyObjectId() throws Exception {
+    FetchAction.Input inputParams = new FetchAction.Input();
+    inputParams.label = label;
+    inputParams.object_id = "";
+
+    fetchAction.apply(projectResource, inputParams);
+  }
+
+  @Test(expected = RestApiException.class)
+  public void shouldThrowRestApiExceptionWhenPocessingInterrupted()
+      throws RestApiException, InterruptedException, ExecutionException,
+          RemoteConfigurationMissingException {
+    FetchAction.Input inputParams = new FetchAction.Input();
+    inputParams.label = label;
+    inputParams.object_id = objectId;
+
+    doThrow(new InterruptedException()).when(fetchService).fetch(any(), any(), any());
+
+    fetchAction.apply(projectResource, inputParams);
+  }
+
+  @Test(expected = UnprocessableEntityException.class)
+  public void shouldThrowRestApiExceptionWhenNoSurceForGivenLabel()
+      throws RestApiException, InterruptedException, ExecutionException,
+          RemoteConfigurationMissingException {
+    FetchAction.Input inputParams = new FetchAction.Input();
+    inputParams.label = "non-existing-label";
+    inputParams.object_id = objectId;
+
+    doThrow(new RemoteConfigurationMissingException(""))
+        .when(fetchService)
+        .fetch(any(), any(), any());
+
+    fetchAction.apply(projectResource, inputParams);
+  }
+
+  @Test(expected = RestApiException.class)
+  public void shouldThrowRestApiExceptionWhenIssueDuringPocessing()
+      throws RestApiException, InterruptedException, ExecutionException,
+          RemoteConfigurationMissingException {
+    FetchAction.Input inputParams = new FetchAction.Input();
+    inputParams.label = label;
+    inputParams.object_id = objectId;
+
+    doThrow(new ExecutionException(new RuntimeException()))
+        .when(fetchService)
+        .fetch(any(), any(), any());
+
+    fetchAction.apply(projectResource, inputParams);
+  }
+
+  @Test(expected = RestApiException.class)
+  public void shouldThrowRestApiExceptionWhenIssueWithUrlParam()
+      throws RestApiException, InterruptedException, ExecutionException,
+          RemoteConfigurationMissingException {
+    FetchAction.Input inputParams = new FetchAction.Input();
+    inputParams.label = label;
+    inputParams.object_id = objectId;
+
+    doThrow(new IllegalStateException()).when(fetchService).fetch(any(), any(), any());
+
+    fetchAction.apply(projectResource, inputParams);
+  }
+}
