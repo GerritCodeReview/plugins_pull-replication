bff5bd486df977b820a86733c9a8bfd37b472ed1
Marcin Czech <maczech@gmail.com>
2020-03-05 00:53:45 +0100

    Move hardcoded HTTP connection parameters to the replication.config file
    Feature: Issue 11605
    Change-Id: I86419d1854ee405bae3abe947ed6a898c5c1d75b

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/client/HttpClientProvider.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/client/HttpClientProvider.java
@@ -14,23 +14,38 @@
 
 package com.googlesource.gerrit.plugins.replication.pull.client;
 
+import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.ProvisionException;
+import com.googlesource.gerrit.plugins.replication.ReplicationFileBasedConfig;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.eclipse.jgit.lib.Config;
 
 /** Provides an HTTP client with SSL capabilities. */
 public class HttpClientProvider implements Provider<CloseableHttpClient> {
-  private static final int CONNECTIONS_PER_ROUTE = 100;
-
-  // Up to 2 target instances with the max number of connections per host:
-  private static final int MAX_CONNECTIONS = 2 * CONNECTIONS_PER_ROUTE;
-
-  private static final int MAX_CONNECTION_INACTIVITY_MS = 10000;
+  private static final int DEFAULT_CONNECTIONS_PER_ROUTE = 100;
+  private static final int DEFAULT_MAX_CONNECTION_INACTIVITY_MS = 10000;
   private static final int DEFAULT_TIMEOUT_MS = 5000;
+
+  private int maxConnectionsPerRoute;
+  private int maxConnections;
+  private int connectionTimeout;
+  private int idleTimeout;
+
+  @Inject
+  HttpClientProvider(ReplicationFileBasedConfig replicationConfig) {
+    Config cfg = replicationConfig.getConfig();
+    this.maxConnectionsPerRoute =
+        cfg.getInt("replication", "maxConnectionsPerRoute", DEFAULT_CONNECTIONS_PER_ROUTE);
+    this.maxConnections = cfg.getInt("replication", "maxConnections", 2 * maxConnectionsPerRoute);
+    this.connectionTimeout = cfg.getInt("replication", "connectionTimeout", DEFAULT_TIMEOUT_MS);
+    this.idleTimeout =
+        cfg.getInt("replication", "idleTimeout", DEFAULT_MAX_CONNECTION_INACTIVITY_MS);
+  }
 
   @Override
   public CloseableHttpClient get() {
@@ -46,17 +61,17 @@ public class HttpClientProvider implements Provider<CloseableHttpClient> {
 
   private RequestConfig customRequestConfig() {
     return RequestConfig.custom()
-        .setConnectTimeout(DEFAULT_TIMEOUT_MS)
-        .setSocketTimeout(DEFAULT_TIMEOUT_MS)
-        .setConnectionRequestTimeout(DEFAULT_TIMEOUT_MS)
+        .setConnectTimeout(connectionTimeout)
+        .setSocketTimeout(connectionTimeout)
+        .setConnectionRequestTimeout(connectionTimeout)
         .build();
   }
 
   private HttpClientConnectionManager customConnectionManager() throws Exception {
     PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager();
-    connManager.setDefaultMaxPerRoute(CONNECTIONS_PER_ROUTE);
-    connManager.setMaxTotal(MAX_CONNECTIONS);
-    connManager.setValidateAfterInactivity(MAX_CONNECTION_INACTIVITY_MS);
+    connManager.setDefaultMaxPerRoute(maxConnectionsPerRoute);
+    connManager.setMaxTotal(maxConnections);
+    connManager.setValidateAfterInactivity(idleTimeout);
     return connManager;
   }
 }
