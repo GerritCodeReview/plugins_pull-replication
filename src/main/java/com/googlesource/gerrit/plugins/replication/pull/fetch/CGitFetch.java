a15853a30d9225897469296725d1cf531ec81159
Marcin Czech <maczech@gmail.com>
2020-07-01 00:28:10 +0200

    Add native git fetch support
    Current implementation uses JGit to fetch refs from remote. JGit fetch
    implementation performance is lower then native git fetch. Add support
    for native git to workaround performance issues.
    
    Feature: Issue 11605
    Change-Id: Ib1f018a339e3fb13be83652237c8a4fb1fd4f1fa

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/fetch/CGitFetch.java
@@ -0,0 +1,92 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication.pull.fetch;
+
+import static com.googlesource.gerrit.plugins.replication.pull.PullReplicationLogger.repLog;
+
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import org.eclipse.jgit.errors.TransportException;
+import org.eclipse.jgit.lib.RefUpdate;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.transport.RefSpec;
+import org.eclipse.jgit.transport.RemoteConfig;
+import org.eclipse.jgit.transport.URIish;
+
+public class CGitFetch implements Fetch {
+
+  private File localProjectDirectory;
+  private URIish uri;
+  private int timeout;
+
+  @Inject
+  public CGitFetch(RemoteConfig config, @Assisted URIish uri, @Assisted Repository git) {
+
+    this.localProjectDirectory = git.getDirectory();
+    this.uri = uri;
+    this.timeout = config.getTimeout();
+  }
+
+  @Override
+  public List<RefUpdateState> fetch(List<RefSpec> refs) throws IOException {
+    String refsStr = refs.stream().map(s -> s.toString()).collect(Collectors.joining(" "));
+    String[] command = new String[] {"git", "fetch", uri.toASCIIString(), refsStr};
+
+    ProcessBuilder pb = new ProcessBuilder().command(command).directory(localProjectDirectory);
+    repLog.info("Fetch references {} from {}", refsStr, uri);
+    Process process = pb.start();
+
+    try {
+      boolean isFinished = waitForTaskToFinish(process);
+      if (!isFinished) {
+        throw new TransportException(
+            String.format("Timeout exception during the fetch from: %s, refs: %s", uri, refsStr));
+      }
+      if (process.exitValue() != 0) {
+        String errorMessage =
+            new BufferedReader(new InputStreamReader(process.getErrorStream()))
+                .lines()
+                .collect(Collectors.joining("\n"));
+        throw new TransportException(
+            String.format("Cannot fetch from %s, error message: %s}", uri, errorMessage));
+      }
+
+      return refs.stream()
+          .map(
+              value -> {
+                return new RefUpdateState(value.getSource(), RefUpdate.Result.NEW);
+              })
+          .collect(Collectors.toList());
+    } catch (InterruptedException e) {
+      repLog.error("Thread interrupted during the fetch from: {}, refs: {}", uri, refsStr);
+      throw new IllegalStateException(e);
+    }
+  }
+
+  public boolean waitForTaskToFinish(Process process) throws InterruptedException {
+    if (timeout == 0) {
+      process.waitFor();
+      return true;
+    }
+    return process.waitFor(timeout, TimeUnit.SECONDS);
+  }
+}
