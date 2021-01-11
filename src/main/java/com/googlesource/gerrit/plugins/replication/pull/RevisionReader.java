9fc5342e7065b138d60380f3c2c39d93468fe359
Marcin Czech <maczech@gmail.com>
2021-01-11 15:00:32 +0100

    Send small refs as a fetch call payload
    If total size of the ref is smaller than 'payloadMaxRefSize'
    parameter send all objects for that ref as a REST Api call
    payload. Otherwise use git fetch operation.
    
    Change-Id: I3b39bd57120016617128f841a62c66341b968fb0

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/RevisionReader.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/RevisionReader.java
@@ -21,12 +21,14 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.googlesource.gerrit.plugins.replication.pull.api.data.RevisionData;
 import com.googlesource.gerrit.plugins.replication.pull.api.data.RevisionObjectData;
+import com.googlesource.gerrit.plugins.replication.pull.api.exception.RefUpdateException;
 import java.io.IOException;
 import java.util.List;
 import org.eclipse.jgit.errors.CorruptObjectException;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.LargeObjectException;
 import org.eclipse.jgit.errors.MissingObjectException;
-import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectLoader;
 import org.eclipse.jgit.lib.Ref;
@@ -37,45 +39,69 @@ import org.eclipse.jgit.treewalk.TreeWalk;
 
 @Singleton
 public class RevisionReader {
+  private static final Long DEFAULT_MAX_REF_SIZE = 500000L;
   private GitRepositoryManager gitRepositoryManager;
+  private Long maxRefSize;
 
   @Inject
-  public RevisionReader(GitRepositoryManager gitRepositoryManager) {
+  public RevisionReader(GitRepositoryManager gitRepositoryManager, Config cfg) {
     this.gitRepositoryManager = gitRepositoryManager;
+    this.maxRefSize = cfg.getLong("replication", "payloadMaxRefSize", DEFAULT_MAX_REF_SIZE);
   }
 
   public RevisionData read(Project.NameKey project, String refName)
       throws MissingObjectException, IncorrectObjectTypeException, CorruptObjectException,
-          IOException {
+          IOException, LargeObjectException, RefUpdateException {
     try (Repository git = gitRepositoryManager.openRepository(project)) {
       Ref head = git.exactRef(refName);
+      if (head == null) {
+        throw new RefUpdateException(
+            String.format("Cannot find ref %s in project %s", refName, project.get()));
+      }
+      Long totalRefSize = 0l;
 
       ObjectLoader commitLoader = git.open(head.getObjectId());
+
+      verifySize(totalRefSize, commitLoader, head.getObjectId());
+      totalRefSize += commitLoader.getSize();
+
       RevCommit commit = RevCommit.parse(commitLoader.getCachedBytes());
+
       RevisionObjectData commitRev =
           new RevisionObjectData(commit.getType(), commitLoader.getCachedBytes());
 
       RevTree tree = commit.getTree();
       ObjectLoader treeLoader = git.open(commit.getTree().toObjectId());
+      verifySize(totalRefSize, treeLoader, head.getObjectId());
+      totalRefSize += treeLoader.getSize();
+
       RevisionObjectData treeRev =
           new RevisionObjectData(tree.getType(), treeLoader.getCachedBytes());
 
       List<RevisionObjectData> blobs = Lists.newLinkedList();
       try (TreeWalk walk = new TreeWalk(git)) {
         walk.addTree(tree);
+        walk.setRecursive(true);
         while (walk.next()) {
-          ObjectId blobObjectId = walk.getObjectId(0);
-          if (!blobObjectId.equals(commit.getId())) {
-            ObjectLoader blobLoader = git.open(blobObjectId);
-            if (blobLoader.getType() == Constants.OBJ_BLOB) {
-              RevisionObjectData rev =
-                  new RevisionObjectData(blobLoader.getType(), blobLoader.getCachedBytes());
-              blobs.add(rev);
-            }
+          ObjectId objectId = walk.getObjectId(0);
+          if (!objectId.equals(commit.getId())) {
+            ObjectLoader objectLoader = git.open(objectId);
+            verifySize(totalRefSize, objectLoader, head.getObjectId());
+            totalRefSize += objectLoader.getSize();
+
+            RevisionObjectData rev =
+                new RevisionObjectData(objectLoader.getType(), objectLoader.getCachedBytes());
+            blobs.add(rev);
           }
         }
       }
       return new RevisionData(commitRev, treeRev, blobs);
+    }
+  }
+
+  private void verifySize(Long totalRefSize, ObjectLoader loader, ObjectId objectId) {
+    if (loader.isLarge() || (totalRefSize + loader.getSize()) > maxRefSize) {
+      throw new LargeObjectException(objectId);
     }
   }
 }
