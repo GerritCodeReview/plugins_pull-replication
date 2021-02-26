f96c92a87a274e71fcc734331fc03b3e479bc537
Luca Milanesio <luca.milanesio@gmail.com>
2021-02-26 23:23:54 +0000

    Do not inject LocalDiskRepositoryManager in ApplyObject
    The LocalDiskRepositoryManager should never be directly
    inject but rather leave the Guice injection decide
    which implementation should be used.
    
    Change-Id: Id1bfb67de427b1dd2261d15dccf867f9065e5f68

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/fetch/ApplyObject.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/fetch/ApplyObject.java
@@ -16,7 +16,6 @@ package com.googlesource.gerrit.plugins.replication.pull.fetch;
 
 import com.google.gerrit.entities.Project;
 import com.google.gerrit.server.git.GitRepositoryManager;
-import com.google.gerrit.server.git.LocalDiskRepositoryManager;
 import com.google.inject.Inject;
 import com.googlesource.gerrit.plugins.replication.pull.api.data.RevisionData;
 import com.googlesource.gerrit.plugins.replication.pull.api.data.RevisionObjectData;
@@ -34,7 +33,7 @@ public class ApplyObject {
   private final GitRepositoryManager gitManager;
 
   @Inject
-  public ApplyObject(LocalDiskRepositoryManager gitManager) {
+  public ApplyObject(GitRepositoryManager gitManager) {
     this.gitManager = gitManager;
   }
 
