2d019e95dbc3242d777fd8ce06d638822fafdf1d
Marcin Czech <maczech@gmail.com>
2021-09-10 16:23:05 +0200

    Allow using pull-replication on replica nodes
    Gerrit replica nodes do not bind the REST-API servlet: the
    standard binding of the pull-replication API would not work
    at all.
    
    Define on replica node a special servlet filter that is
    filtering exactly the pull-replication API and serves exactly
    the same content in the same way.
    
    Bug: Issue 14982
    Change-Id: Ifa148c4865527b67260e7d4bb1c91d1472512af9

---
+++ src/test/java/com/googlesource/gerrit/plugins/replication/pull/api/FetchActionIT.java
@@ -0,0 +1,45 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication.pull.api;
+
+import com.google.gerrit.acceptance.config.GerritConfig;
+import com.google.gerrit.extensions.restapi.Url;
+import org.junit.Test;
+
+public class FetchActionIT extends ActionITBase {
+
+  @Test
+  @GerritConfig(name = "container.replica", value = "true")
+  public void shouldFetchRefWhenNodeIsAReplica() throws Exception {
+    String refName = createRef();
+    String sendObjectPayload =
+        "{\"label\":\""
+            + TEST_REPLICATION_REMOTE
+            + "\", \"ref_name\": \""
+            + refName
+            + "\", \"async\":false}";
+
+    httpClientFactory
+        .create(source)
+        .execute(createRequest(sendObjectPayload), assertHttpResponseCode(201), getContext());
+  }
+
+  @Override
+  protected String getURL() {
+    return String.format(
+        "%s/a/projects/%s/pull-replication~fetch",
+        adminRestSession.url(), Url.encode(project.get()));
+  }
+}
