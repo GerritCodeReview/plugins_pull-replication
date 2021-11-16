a34d06f877e51dbe6267051758b34e9817c09cd8
Fabio Ponciroli <ponch78@gmail.com>
2021-11-16 11:17:13 +0100

    Allow project deletion on replicas
    Bug: Issue 15270
    Change-Id: Ie6843ca21d96e776b55011a320d5b963e4d35b9c

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/api/ProjectDeletionAction.java
@@ -0,0 +1,68 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication.pull.api;
+
+import com.google.gerrit.extensions.api.access.PluginPermission;
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.extensions.restapi.BadRequestException;
+import com.google.gerrit.extensions.restapi.ResourceConflictException;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
+import com.google.gerrit.server.permissions.PermissionBackend;
+import com.google.gerrit.server.project.ProjectResource;
+import com.google.inject.Inject;
+import com.googlesource.gerrit.plugins.replication.LocalFS;
+import com.googlesource.gerrit.plugins.replication.pull.GerritConfigOps;
+import java.util.Optional;
+import org.eclipse.jgit.transport.URIish;
+
+class ProjectDeletionAction
+    implements RestModifyView<ProjectResource, ProjectDeletionAction.DeleteInput> {
+  private static final PluginPermission DELETE_PROJECT =
+      new PluginPermission("delete-project", "deleteProject");
+
+  static class DeleteInput {}
+
+  private final GerritConfigOps gerritConfigOps;
+  private final PermissionBackend permissionBackend;
+
+  @Inject
+  ProjectDeletionAction(GerritConfigOps gerritConfigOps, PermissionBackend permissionBackend) {
+    this.gerritConfigOps = gerritConfigOps;
+    this.permissionBackend = permissionBackend;
+  }
+
+  @Override
+  public Response<?> apply(ProjectResource projectResource, DeleteInput input)
+      throws AuthException, BadRequestException, ResourceConflictException, Exception {
+
+    permissionBackend.user(projectResource.getUser()).check(DELETE_PROJECT);
+
+    Optional<URIish> maybeRepoURI =
+        gerritConfigOps.getGitRepositoryURI(String.format("%s.git", projectResource.getName()));
+
+    if (maybeRepoURI.isPresent()) {
+      if (new LocalFS(maybeRepoURI.get()).deleteProject(projectResource.getNameKey())) {
+        return Response.ok();
+      }
+      throw new UnprocessableEntityException(
+          String.format("Could not delete project %s", projectResource.getName()));
+    }
+    throw new ResourceNotFoundException(
+        String.format("Could not compute URI for repo: %s", projectResource.getName()));
+  }
+}
