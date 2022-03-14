52aa116303428800008e5003a61d94b770618586
Marcin Czech <maczech@gmail.com>
2022-03-14 09:10:07 +0100

    Use stream events to fetch refs and create repositories
    Add stream events listener to trigger git fetch references and to create
    repositories. This functionality with combination with events-broker and
    multi-site provides backfill mechanism for REST Api calls missed when
    node was unreachable.
    
    Bug: Issue 15636
    Change-Id: I573d4b403878ad781eeabf2b88416f3b29c25787

---
+++ src/test/java/com/googlesource/gerrit/plugins/replication/pull/event/StreamEventListenerTest.java
@@ -0,0 +1,111 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication.pull.event;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.server.data.RefUpdateAttribute;
+import com.google.gerrit.server.events.Event;
+import com.google.gerrit.server.events.ProjectCreatedEvent;
+import com.google.gerrit.server.events.RefUpdatedEvent;
+import com.google.gerrit.server.git.WorkQueue;
+import com.google.gerrit.server.permissions.PermissionBackendException;
+import com.googlesource.gerrit.plugins.replication.pull.api.FetchAction.FetchJob;
+import com.googlesource.gerrit.plugins.replication.pull.api.FetchCommand;
+import com.googlesource.gerrit.plugins.replication.pull.api.ProjectInitializationAction;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StreamEventListenerTest {
+
+	private static final String TEST_PROJECT = "test-project";
+	private static final String INSTANCE_ID = "node_instance_id";
+	private static final String REMOTE_INSTANCE_ID = "remote_node_instance_id";
+	
+	@Mock
+	private FetchCommand fetchCommand;
+	@Mock
+	private ProjectInitializationAction projectInitializationAction;
+	@Mock
+	private WorkQueue workQueue;
+	@Mock
+	private ScheduledExecutorService executor;
+	
+	private StreamEventListener objectUnderTest;
+
+	@Before
+	public void setup() {
+		when(workQueue.getDefaultQueue()).thenReturn(executor);
+		objectUnderTest = new StreamEventListener(INSTANCE_ID, fetchCommand, projectInitializationAction, workQueue);
+	}
+	
+	@Test
+	public void shouldSkipEventsGeneratedByTheSameInstance() {
+		Event event = new RefUpdatedEvent();
+		event.instanceId = INSTANCE_ID;
+		objectUnderTest.onEvent(event);
+		
+		verify(executor, never()).submit(any(Runnable.class));
+	}
+	
+	@Test
+	public void shouldScheduleFetchJobForRefUpdateEvent() {
+		RefUpdatedEvent event = new RefUpdatedEvent();
+		RefUpdateAttribute refUpdate = new RefUpdateAttribute();
+		refUpdate.refName = "refs/changes/01/1/1";
+		refUpdate.project = TEST_PROJECT;
+		
+		event.instanceId = REMOTE_INSTANCE_ID;
+		event.refUpdate = () -> refUpdate; 
+		
+		objectUnderTest.onEvent(event);
+		
+		verify(executor, times(1)).submit(any(FetchJob.class));
+	}
+	
+	@Test
+	public void shouldCreateProjectForProjectCreatedEvent() throws AuthException, PermissionBackendException {
+		ProjectCreatedEvent event = new ProjectCreatedEvent();
+		event.instanceId = REMOTE_INSTANCE_ID;
+		event.projectName = TEST_PROJECT;
+		
+		objectUnderTest.onEvent(event);
+		
+		verify(projectInitializationAction, times(1)).initProject(String.format("%s.git", TEST_PROJECT));
+	}
+	
+	@Test
+	public void shouldScheduleAllRefsFetchForProjectCreatedEvent() {
+		ProjectCreatedEvent event = new ProjectCreatedEvent();
+		event.instanceId = REMOTE_INSTANCE_ID;
+		event.projectName = TEST_PROJECT;
+		
+		objectUnderTest.onEvent(event);
+		
+		verify(executor, times(1)).submit(any(FetchJob.class));
+	}
+}
