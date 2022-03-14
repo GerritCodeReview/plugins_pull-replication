5783ff6c5503b3209d7cd6a83ecc766fe2c24330
Marcin Czech <maczech@gmail.com>
2022-03-14 19:14:34 +0100

    Refactor FetchJob to an assisted injection factory
    FetchJob class is used in multiple places. Add guice assisted injection
    factory to allow code reuse and unit testing
    
    Bug: Issue 15636
    Change-Id: I1544575c17b977188765e20f70ab7a9053dcd5b3

--- src/test/java/com/googlesource/gerrit/plugins/replication/pull/event/StreamEventListenerTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/replication/pull/event/StreamEventListenerTest.java
@@ -14,12 +14,15 @@
 
 package com.googlesource.gerrit.plugins.replication.pull.event;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.google.gerrit.entities.Project;
 import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.server.data.RefUpdateAttribute;
 import com.google.gerrit.server.events.Event;
@@ -27,20 +30,25 @@ import com.google.gerrit.server.events.ProjectCreatedEvent;
 import com.google.gerrit.server.events.RefUpdatedEvent;
 import com.google.gerrit.server.git.WorkQueue;
 import com.google.gerrit.server.permissions.PermissionBackendException;
-import com.googlesource.gerrit.plugins.replication.pull.api.FetchAction.FetchJob;
+import com.googlesource.gerrit.plugins.replication.pull.FetchOne;
+import com.googlesource.gerrit.plugins.replication.pull.api.FetchAction.Input;
 import com.googlesource.gerrit.plugins.replication.pull.api.FetchCommand;
+import com.googlesource.gerrit.plugins.replication.pull.api.FetchJob;
 import com.googlesource.gerrit.plugins.replication.pull.api.ProjectInitializationAction;
 import java.util.concurrent.ScheduledExecutorService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StreamEventListenerTest {
 
-  private static final String TEST_PROJECT = "test-project";
+  private static final String TEST_REF_NAME = "refs/changes/01/1/1";
+  private static final Project.NameKey TEST_PROJECT = Project.nameKey("test-project");
   private static final String INSTANCE_ID = "node_instance_id";
   private static final String REMOTE_INSTANCE_ID = "remote_node_instance_id";
 
@@ -48,14 +56,19 @@ public class StreamEventListenerTest {
   @Mock private ProjectInitializationAction projectInitializationAction;
   @Mock private WorkQueue workQueue;
   @Mock private ScheduledExecutorService executor;
+  @Mock private FetchJob fetchJob;
+  @Mock private FetchJob.Factory fetchJobFactory;
+  @Captor ArgumentCaptor<Input> inputCaptor;
 
   private StreamEventListener objectUnderTest;
 
   @Before
   public void setup() {
     when(workQueue.getDefaultQueue()).thenReturn(executor);
+    when(fetchJobFactory.create(any(), any())).thenReturn(fetchJob);
     objectUnderTest =
-        new StreamEventListener(INSTANCE_ID, fetchCommand, projectInitializationAction, workQueue);
+        new StreamEventListener(
+            INSTANCE_ID, projectInitializationAction, workQueue, fetchJobFactory);
   }
 
   @Test
@@ -71,13 +84,19 @@ public class StreamEventListenerTest {
   public void shouldScheduleFetchJobForRefUpdateEvent() {
     RefUpdatedEvent event = new RefUpdatedEvent();
     RefUpdateAttribute refUpdate = new RefUpdateAttribute();
-    refUpdate.refName = "refs/changes/01/1/1";
-    refUpdate.project = TEST_PROJECT;
+    refUpdate.refName = TEST_REF_NAME;
+    refUpdate.project = TEST_PROJECT.get();
 
     event.instanceId = REMOTE_INSTANCE_ID;
     event.refUpdate = () -> refUpdate;
 
     objectUnderTest.onEvent(event);
+
+    verify(fetchJobFactory, times(1)).create(eq(TEST_PROJECT), inputCaptor.capture());
+
+    Input input = inputCaptor.getValue();
+    assertThat(input.label).isEqualTo(REMOTE_INSTANCE_ID);
+    assertThat(input.refName).isEqualTo(TEST_REF_NAME);
 
     verify(executor, times(1)).submit(any(FetchJob.class));
   }
@@ -87,21 +106,27 @@ public class StreamEventListenerTest {
       throws AuthException, PermissionBackendException {
     ProjectCreatedEvent event = new ProjectCreatedEvent();
     event.instanceId = REMOTE_INSTANCE_ID;
-    event.projectName = TEST_PROJECT;
+    event.projectName = TEST_PROJECT.get();
 
     objectUnderTest.onEvent(event);
 
     verify(projectInitializationAction, times(1))
-        .initProject(String.format("%s.git", TEST_PROJECT));
+        .initProject(String.format("%s.git", TEST_PROJECT.get()));
   }
 
   @Test
   public void shouldScheduleAllRefsFetchForProjectCreatedEvent() {
     ProjectCreatedEvent event = new ProjectCreatedEvent();
     event.instanceId = REMOTE_INSTANCE_ID;
-    event.projectName = TEST_PROJECT;
+    event.projectName = TEST_PROJECT.get();
 
     objectUnderTest.onEvent(event);
+
+    verify(fetchJobFactory, times(1)).create(eq(TEST_PROJECT), inputCaptor.capture());
+
+    Input input = inputCaptor.getValue();
+    assertThat(input.label).isEqualTo(REMOTE_INSTANCE_ID);
+    assertThat(input.refName).isEqualTo(FetchOne.ALL_REFS);
 
     verify(executor, times(1)).submit(any(FetchJob.class));
   }
