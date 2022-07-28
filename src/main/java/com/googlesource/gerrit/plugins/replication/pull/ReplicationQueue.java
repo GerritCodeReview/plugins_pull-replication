1e2d2c57152dd22bd80a057c21c2016f74148843
Marcin Czech <maczech@gmail.com>
2022-07-28 21:22:52 +0200

    Fix issue with fetching all refs after project creation
    Replace `refs/*` with `..all..` to correctly fetch all refs after
    project creation
    
    Change-Id: I0aa703ff7900debc35a1427c04e22dfe774bb2a3

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/ReplicationQueue.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/ReplicationQueue.java
@@ -402,7 +402,7 @@ public class ReplicationQueue
       throws IOException, ClientProtocolException {
     HttpResult initProjectResult = fetchClient.initProject(project, uri);
     if (initProjectResult.isSuccessful()) {
-      result = fetchClient.callFetch(project, "refs/*", uri);
+      result = fetchClient.callFetch(project, FetchOne.ALL_REFS, uri);
     } else {
       String errorMessage = initProjectResult.getMessage().map(e -> " - Error: " + e).orElse("");
       repLog.error("Cannot create project " + project + errorMessage);
