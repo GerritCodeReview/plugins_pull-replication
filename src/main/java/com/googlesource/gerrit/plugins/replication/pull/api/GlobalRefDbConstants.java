de7982fedbba655a0073719fb6637dfe016ed801
Luca Milanesio <luca.milanesio@gmail.com>
2022-11-21 22:50:45 +0000

    Decouple the GitRepositoryManager used from the LocalDiskRepositoryManager
    The repository manager to use may be different from the LocalDiskRepositoryManager.
    Use a generic named binding so that it would be possible to replace
    the implementation by configuration.
    
    In case there is no binding of the LocalDiskRepositoryManager,
    make the binding explicitly in the PullReplicationModule so that
    the whole chain of injections will still work as before.
    
    Change-Id: I49257b1121016cf0a7d157a117420299bd28d437

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/api/GlobalRefDbConstants.java
@@ -0,0 +1,23 @@
+// Copyright (C) 2022 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication.pull.api;
+
+/* Weak dependency with the named annotation used in the global-refdb,
+ * for avoiding the hassle of depending from another repository just for a
+ * single string
+ */
+public interface GlobalRefDbConstants {
+  public static final String LOCAL_DISK_REPOSITORY_MANAGER = "local_disk_repository_manager";
+}
