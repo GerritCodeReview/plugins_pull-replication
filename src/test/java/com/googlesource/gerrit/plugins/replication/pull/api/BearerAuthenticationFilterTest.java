23e15d2e593436deaf0998269fda65285bfb08eb
Álvaro Vilaplana García <alvaro.vilaplana@gmail.com>
2022-12-27 12:14:00 +0000

    Allow anonymous clones when pull-replication plugin is enabled.
    When the command `git clone` is executed over http, behind the scenes it does a request: `/info/refs?service=git-upload-pack`. Currently `BearerAuthenticationFilter.java` filters ALL the `upload pack` requests and therefore it checks the Bearer token header. This feature was added to allow Bearer Token Authentication when `git fetch over http` is executed from replica instances.
    
    The fix allows to discriminate the `upload pack` requests, therefore Bearer token header checking will be done just in the case of `git fetch over http` from replica instances.
    
    Bug: Issue 16548
    Change-Id: I9873d92c88b8eb00c895a8e1cce18cae94199ac9

--- src/test/java/com/googlesource/gerrit/plugins/replication/pull/api/BearerAuthenticationFilterTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/replication/pull/api/BearerAuthenticationFilterTest.java
@@ -16,7 +16,7 @@ package com.googlesource.gerrit.plugins.replication.pull.api;
 
 import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
 import static org.mockito.Mockito.atMost;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -58,6 +58,7 @@ public class BearerAuthenticationFilterTest {
     final String bearerToken = "some-bearer-token";
     when(httpServletRequest.getRequestURI()).thenReturn(uri);
     queryStringMaybe.ifPresent(qs -> when(httpServletRequest.getQueryString()).thenReturn(qs));
+    when(httpServletRequest.getHeader("Plugin")).thenReturn(pluginName);
     when(httpServletRequest.getHeader("Authorization"))
         .thenReturn(String.format("Bearer %s", bearerToken));
     when(threadLocalRequestContextProvider.get()).thenReturn(threadLocalRequestContext);
@@ -70,6 +71,7 @@ public class BearerAuthenticationFilterTest {
     verify(httpServletRequest, atMost(2)).getRequestURI();
     verify(httpServletRequest, atMost(1)).getQueryString();
     verify(httpServletRequest).getHeader("Authorization");
+    verify(httpServletRequest, atMost(1)).getHeader("Plugin");
     verify(threadLocalRequestContextProvider).get();
     verify(session).get();
     verify(webSession).setAccessPathOk(AccessPath.REST_API, true);
@@ -108,14 +110,32 @@ public class BearerAuthenticationFilterTest {
   }
 
   @Test
-  public void shouldAuthenticateWhenGitUploadPacket() throws ServletException, IOException {
+  public void shouldAuthenticateWhenGitUploadPackFromPlugin() throws ServletException, IOException {
     authenticateAndFilter("any-prefix/git-upload-pack", NO_QUERY_PARAMETERS);
   }
 
   @Test
-  public void shouldAuthenticateWhenGitUploadPacketInQueryParameter()
+  public void shouldAuthenticateWhenGitUploadPackInQueryParameterFromPlugin()
       throws ServletException, IOException {
     authenticateAndFilter("any-prefix", GIT_UPLOAD_PACK_QUERY_PARAMETER);
+  }
+
+  @Test
+  public void shouldGoNextInChainWhenGitUploadPackNotFromPlugin()
+      throws ServletException, IOException {
+    when(httpServletRequest.getRequestURI()).thenReturn("any-prefix/git-upload-pack");
+
+    final BearerAuthenticationFilter filter =
+        new BearerAuthenticationFilter(
+            session,
+            pluginName,
+            pluginUser,
+            threadLocalRequestContextProvider,
+            "some-bearer-token");
+    filter.doFilter(httpServletRequest, httpServletResponse, filterChain);
+
+    verify(httpServletRequest, never()).getHeader("Authorization");
+    verify(filterChain).doFilter(httpServletRequest, httpServletResponse);
   }
 
   @Test
@@ -187,7 +207,7 @@ public class BearerAuthenticationFilterTest {
             "some-bearer-token");
     filter.doFilter(httpServletRequest, httpServletResponse, filterChain);
 
-    verify(httpServletRequest, times(2)).getRequestURI();
+    verify(httpServletRequest, never()).getHeader("Authorization");
     verify(filterChain).doFilter(httpServletRequest, httpServletResponse);
   }
 
@@ -206,7 +226,7 @@ public class BearerAuthenticationFilterTest {
             "some-bearer-token");
     filter.doFilter(httpServletRequest, httpServletResponse, filterChain);
 
-    verify(httpServletRequest).getRequestURI();
+    verify(httpServletRequest, never()).getHeader("Authorization");
     verify(filterChain).doFilter(httpServletRequest, httpServletResponse);
   }
 }
