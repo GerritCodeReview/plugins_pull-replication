ee02e18902fc358088a5ebf213c88493f55a9a96
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-01-09 17:35:38 +0000

    Add Init phase in primary and replica.
    Add push replication in the primary.
    
    Also configure the initial ACLs of the All-Projects repo
    for allowing the pull-replication internal user to access
    all refs of All-Users repository. Without that ACL the
    pull-replication would corrupt the repository because it
    would only replicate the refs/meta/config but not the
    groups and users.
    
    Change-Id: Id27d1af7208250795c6a723624890e3555b42160

--- example-setup/entrypoint.sh
+++ example-setup/entrypoint.sh
@@ -2,10 +2,17 @@
 
 function setup_replication_config {
 
-  echo "Replacing variables for file /var/gerrit/etc/replication.config.template"
-  cat /var/gerrit/etc/replication.config.template | envsubst | sed 's/#{name}#/${name}/g' > /var/gerrit/etc/replication.config
+  if $REPLICA;
+  then
+    echo "Replacing variables for file /var/gerrit/etc/replication.config.template"
+    cat /var/gerrit/etc/replication-replica.config.template | envsubst | sed 's/#{name}#/${name}/g' > /var/gerrit/etc/replication.config
+    cat /var/gerrit/etc/replication.config
+  else
+    echo "Replacing variables for file /var/gerrit/etc/replication.config.template"
+    cat /var/gerrit/etc/replication-primary.config.template | envsubst | sed 's/#{name}#/${name}/g' > /var/gerrit/etc/replication.config
+    cat /var/gerrit/etc/replication.config
+  fi
 
-  cat /var/gerrit/etc/replication.config
 }
 
 function setup_gerrit_config {
@@ -15,6 +22,22 @@ function setup_gerrit_config {
 
 setup_replication_config
 setup_gerrit_config
+
+echo "Init phase..."
+java -jar /var/gerrit/bin/gerrit.war init --no-auto-start --batch --install-all-plugins -d /var/gerrit
+
+echo "Grant access database to the pull-replication internal user ..."
+mkdir /tmp/gerrit-post-init && pushd /tmp/gerrit-post-init
+git clone /var/gerrit/git/All-Projects && cd All-Projects && git fetch origin refs/meta/config && git checkout FETCH_HEAD
+git config -f project.config capability.accessDatabase "group Pull-replication Internal User"
+echo -e "pullreplication:internal-user\tPull-replication Internal User" >> groups
+git config user.name gerrit
+git config user.email gerrit@localhost
+git add project.config groups
+git commit -m 'Added global-capability to pull-replication internal user'
+git push origin HEAD:refs/meta/config
+popd
+rm -Rf /tmp/gerrit-post-init
 
 echo "Running Gerrit ..."
 exec /var/gerrit/bin/gerrit.sh run
