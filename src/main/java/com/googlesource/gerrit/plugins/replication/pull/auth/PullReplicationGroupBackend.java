10e0bd834b0049f3e62556ce6babf14d077d6bbe
Antonio Barone <syntonyze@gmail.com>
2023-01-12 17:46:18 +0100

    Add init step for pull-replication
    Ensure `AccessDatabase` global capability is automatically configured
    when pull-replication is configured to use bearer token.
    
    Bug: Issue 16594
    Change-Id: Iceef41862ce0b17779f533c37f328cfda3b4640d

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/auth/PullReplicationGroupBackend.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/auth/PullReplicationGroupBackend.java
@@ -30,7 +30,7 @@ import java.util.Collection;
 
 /** Backend to expose the pull-replication internal user group membership. */
 @Singleton
-class PullReplicationGroupBackend extends AbstractGroupBackend {
+public class PullReplicationGroupBackend extends AbstractGroupBackend {
   public static final AccountGroup.UUID INTERNAL_GROUP_UUID =
       AccountGroup.uuid("pullreplication:internal-user");
   public static final String INTERNAL_GROUP_NAME = "Pull-replication Internal User";
