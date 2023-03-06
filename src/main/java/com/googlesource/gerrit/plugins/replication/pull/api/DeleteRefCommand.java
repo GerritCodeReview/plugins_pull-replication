8c5eff5d73aa2b2d7ed3addfeb8bdca7bc8aa774
Marcin Czech <maczech@gmail.com>
2023-03-06 12:37:35 +0100

    Fix issue with NPE when ref does not exists
    Delete ref throws NPE when ref marked to be deleted doesn't exists
    in the local repository. This can happen if repo is out of sync
    or ref was delete via different channel( for example via apply object)
    
    Bug: Issue 15873
    Change-Id: Ic945a44268d7998f312f067ed009ae8af221a63f

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/api/DeleteRefCommand.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/api/DeleteRefCommand.java
@@ -32,12 +32,16 @@ import com.googlesource.gerrit.plugins.replication.pull.FetchRefReplicatedEvent;
 import com.googlesource.gerrit.plugins.replication.pull.LocalGitRepositoryManagerProvider;
 import com.googlesource.gerrit.plugins.replication.pull.PullReplicationStateLogger;
 import com.googlesource.gerrit.plugins.replication.pull.ReplicationState;
+import com.googlesource.gerrit.plugins.replication.pull.ReplicationState.RefFetchResult;
 import com.googlesource.gerrit.plugins.replication.pull.fetch.ApplyObject;
 import com.googlesource.gerrit.plugins.replication.pull.fetch.RefUpdateState;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Optional;
 import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.RefUpdate;
+import org.eclipse.jgit.lib.RefUpdate.Result;
 import org.eclipse.jgit.lib.Repository;
 
 public class DeleteRefCommand {
@@ -86,6 +90,18 @@ public class DeleteRefCommand {
                     sourceLabel,
                     ReplicationState.RefFetchResult.SUCCEEDED,
                     RefUpdate.Result.FORCED));
+      } catch (IllegalStateException e) {
+        eventDispatcher
+            .get()
+            .postEvent(
+                new FetchRefReplicatedEvent(
+                    name.get(),
+                    refName,
+                    sourceLabel,
+                    RefFetchResult.NOT_ATTEMPTED,
+                    Result.REJECTED_MISSING_OBJECT));
+        logger.atWarning().withCause(e).log(
+            "Delete ref from %s for project %s, ref name %s failed", sourceLabel, name, refName);
       } catch (PermissionBackendException e) {
         logger.atSevere().withCause(e).log(
             "Unexpected error while trying to delete ref '%s' on project %s and notifying it",
@@ -122,9 +138,16 @@ public class DeleteRefCommand {
   private RefUpdateState deleteRef(Project.NameKey name, String refName) throws IOException {
 
     try (Repository repository = gitManager.openRepository(name)) {
+
+      Ref ref = repository.exactRef(refName);
+      if (Objects.isNull(ref)) {
+        throw new IllegalStateException(
+            String.format("Unable to find ref %s in project %s", refName, name.get()));
+      }
+
       RefUpdate.Result result;
       RefUpdate u = repository.updateRef(refName);
-      u.setExpectedOldObjectId(repository.exactRef(refName).getObjectId());
+      u.setExpectedOldObjectId(ref.getObjectId());
       u.setNewObjectId(ObjectId.zeroId());
       u.setForceUpdate(true);
 
