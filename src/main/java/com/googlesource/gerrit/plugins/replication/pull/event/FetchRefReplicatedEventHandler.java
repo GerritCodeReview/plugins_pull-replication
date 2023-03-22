e1cfabf3cdeb3c8bccf8b5f03fe46bdc6c28b451
Fabio Ponciroli <ponch78@gmail.com>
2023-03-22 17:49:38 +0100

    Evict SSH cache when updating account
    Make sure user SSH cache is evicted for the updated user to make
    sure addition/deletion of SSH keays are reflected in the target node.
    
    Bug: Issue 16596
    Change-Id: I077f50d90a721e32aa3d8cf69c09b7b38809c288

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/event/FetchRefReplicatedEventHandler.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/event/FetchRefReplicatedEventHandler.java
@@ -15,12 +15,16 @@
 package com.googlesource.gerrit.plugins.replication.pull.event;
 
 import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.entities.Account;
 import com.google.gerrit.entities.Change;
 import com.google.gerrit.entities.Project;
 import com.google.gerrit.entities.RefNames;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.config.AllUsersName;
 import com.google.gerrit.server.events.Event;
 import com.google.gerrit.server.events.EventListener;
 import com.google.gerrit.server.index.change.ChangeIndexer;
+import com.google.gerrit.server.ssh.SshKeyCache;
 import com.google.inject.Inject;
 import com.googlesource.gerrit.plugins.replication.pull.Context;
 import com.googlesource.gerrit.plugins.replication.pull.FetchRefReplicatedEvent;
@@ -28,17 +32,28 @@ import com.googlesource.gerrit.plugins.replication.pull.ReplicationState;
 
 public class FetchRefReplicatedEventHandler implements EventListener {
   private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+  private final AllUsersName allUsers;
   private ChangeIndexer changeIndexer;
+  private final SshKeyCache sshKeyCache;
+  private final IdentifiedUser.GenericFactory userFactory;
 
   @Inject
-  FetchRefReplicatedEventHandler(ChangeIndexer changeIndexer) {
+  FetchRefReplicatedEventHandler(
+      ChangeIndexer changeIndexer,
+      AllUsersName allUsers,
+      SshKeyCache sshKeyCache,
+      IdentifiedUser.GenericFactory userFactory) {
     this.changeIndexer = changeIndexer;
+    this.allUsers = allUsers;
+    this.sshKeyCache = sshKeyCache;
+    this.userFactory = userFactory;
   }
 
   @Override
   public void onEvent(Event event) {
     if (event instanceof FetchRefReplicatedEvent && isLocalEvent()) {
       FetchRefReplicatedEvent fetchRefReplicatedEvent = (FetchRefReplicatedEvent) event;
+      handleAllUsersUpdates(fetchRefReplicatedEvent);
       if (!RefNames.isNoteDbMetaRef(fetchRefReplicatedEvent.getRefName())
           || !fetchRefReplicatedEvent
               .getStatus()
@@ -57,6 +72,16 @@ public class FetchRefReplicatedEventHandler implements EventListener {
         logger.atWarning().log(
             "Couldn't get changeId from refName. Skipping indexing of change %s for project %s",
             fetchRefReplicatedEvent.getRefName(), projectNameKey.get());
+      }
+    }
+  }
+
+  private void handleAllUsersUpdates(FetchRefReplicatedEvent fetchRefReplicatedEvent) {
+    Project.NameKey projectNameKey = fetchRefReplicatedEvent.getProjectNameKey();
+    if (allUsers.equals(projectNameKey)) {
+      Account.Id accountId = Account.Id.fromRef(fetchRefReplicatedEvent.getRefName());
+      if (accountId != null) {
+        userFactory.create(accountId).getUserName().ifPresent(sshKeyCache::evict);
       }
     }
   }
