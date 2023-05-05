a5b4bfa0270ed91ba4a52011fbf12b1f70baa1c6
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-05-05 15:00:10 +0100

    Stop creating redundant replication tasks from stream-events
    Currently the broker-based ref-update triggers a replication
    task once the event is received from the topic, even if the
    apply-object was executed successfully beforehand.
    
    To avoid this problem, an apply-objects cache has been introduced to
    store temporally the apply-objects already successfully applied.
    
    Bug: Issue 16885
    Change-Id: Ib864bd0ae337f6b93308dfea155a22e1955409c3

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/ApplyObjectsCacheKey.java
@@ -0,0 +1,18 @@
+package com.googlesource.gerrit.plugins.replication.pull;
+
+import com.google.auto.value.AutoValue;
+
+@AutoValue
+public abstract class ApplyObjectsCacheKey {
+
+  public static ApplyObjectsCacheKey create(
+      String newRev, String refName, String project) {
+    return new AutoValue_ApplyObjectsCacheKey(newRev, refName, project);
+  }
+
+  public abstract String newRev();
+
+  public abstract String refName();
+
+  public abstract String project();
+}
