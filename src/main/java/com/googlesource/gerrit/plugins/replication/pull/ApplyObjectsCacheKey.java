9ea0f48904aaeddb9cc5de99eee58577e47cbf1f
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-05-05 15:00:10 +0100

    wip
    Change-Id: Ib864bd0ae337f6b93308dfea155a22e1955409c3

---
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/ApplyObjectsCacheKey.java
@@ -0,0 +1,18 @@
+package com.googlesource.gerrit.plugins.replication.pull;
+
+import com.google.auto.value.AutoValue;
+import com.google.gerrit.server.data.RefUpdateAttribute;
+
+@AutoValue
+public abstract class ApplyObjectsCacheKey {
+
+  public static ApplyObjectsCacheKey create(String oldRev,String newRev,String refName,String project) {
+    return new AutoValue_ApplyObjectsCacheKey(oldRev, newRev, refName, project);
+  }
+
+  public abstract String oldRev();
+  public abstract String newRev();
+  public abstract String refName();
+  public abstract String project();
+}
+
