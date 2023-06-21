e2fb926cef4138302585cb1dbd248a149b333377
Marcin Czech <maczech@gmail.com>
2023-06-21 18:36:43 +0200

    Introduce `apply-batch-object` REST API endpoint
    Bug: Issue 40015567
    Change-Id: I843902c639829e04c4964b1f6c86602f1e554a6c

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/api/PullReplicationApiModule.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/api/PullReplicationApiModule.java
@@ -29,8 +29,10 @@ public class PullReplicationApiModule extends RestApiModule {
     bind(ApplyObjectAction.class).in(Scopes.SINGLETON);
     bind(ProjectDeletionAction.class).in(Scopes.SINGLETON);
     bind(UpdateHeadAction.class).in(Scopes.SINGLETON);
+    bind(BatchApplyObjectAction.class).in(Scopes.SINGLETON);
     post(PROJECT_KIND, "fetch").to(FetchAction.class);
     post(PROJECT_KIND, "apply-object").to(ApplyObjectAction.class);
+    post(PROJECT_KIND, "batch-apply-object").to(BatchApplyObjectAction.class);
     post(PROJECT_KIND, "apply-objects").to(ApplyObjectsAction.class);
     delete(PROJECT_KIND, "delete-project").to(ProjectDeletionAction.class);
     put(PROJECT_KIND, "HEAD").to(UpdateHeadAction.class);
