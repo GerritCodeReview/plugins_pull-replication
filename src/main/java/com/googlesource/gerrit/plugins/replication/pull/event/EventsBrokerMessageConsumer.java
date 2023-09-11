af7f7adcd5618ee3673854c93f0717cdabc8a4d7
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-09-11 17:17:28 +0100

    Allow subscribing to the topic with custom group id
    In the past, when utilizing pull-replication as a standalone solution
    in conjunction with a broker (inclusive of plugins such as events-kafka,
    events-aws-kinesis, events-gcloud-pubsub), consumer configurations
    relied on the groupId specified within the events-* plugin configuration.
    In cases where no groupId was explicitly defined, the system defaulted to
    using the value "gerrit.instance-id."
    
    Now, thanks to this new feature, the pull-replication plugin offers
    the option to define a group id when the property
    "replication.eventBrokerGroupId" is specified in the replication.conf
    file. Additionally, this feature is applicable when the events-* plugin
    implements the ExtendedBrokerApi interface.
    
    Bug: Issue 299327285
    Change-Id: I33d056ebd3e80b6a4c9d8eb02a0c2c1b591e8eb5

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/event/EventsBrokerMessageConsumer.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/event/EventsBrokerMessageConsumer.java
@@ -14,9 +14,14 @@
 
 package com.googlesource.gerrit.plugins.replication.pull.event;
 
+import static com.googlesource.gerrit.plugins.replication.pull.event.EventsBrokerConsumerModule.STREAM_EVENTS_GROUP_ID;
 import static com.googlesource.gerrit.plugins.replication.pull.event.EventsBrokerConsumerModule.STREAM_EVENTS_TOPIC_NAME;
 
 import com.gerritforge.gerrit.eventbroker.BrokerApi;
+import com.gerritforge.gerrit.eventbroker.ExtendedBrokerApi;
+import com.google.common.base.Strings;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.common.Nullable;
 import com.google.gerrit.extensions.events.LifecycleListener;
 import com.google.gerrit.extensions.registration.DynamicItem;
 import com.google.gerrit.extensions.restapi.AuthException;
@@ -29,22 +34,26 @@ import java.util.function.Consumer;
 
 public class EventsBrokerMessageConsumer implements Consumer<Event>, LifecycleListener {
 
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
   private final DynamicItem<BrokerApi> eventsBroker;
   private final StreamEventListener eventListener;
   private final ShutdownState shutdownState;
   private final String eventsTopicName;
+  private final String groupId;
 
   @Inject
   public EventsBrokerMessageConsumer(
       DynamicItem<BrokerApi> eventsBroker,
       StreamEventListener eventListener,
       ShutdownState shutdownState,
-      @Named(STREAM_EVENTS_TOPIC_NAME) String eventsTopicName) {
+      @Named(STREAM_EVENTS_TOPIC_NAME) String eventsTopicName,
+      @Nullable @Named(STREAM_EVENTS_GROUP_ID) String groupId) {
 
     this.eventsBroker = eventsBroker;
     this.eventListener = eventListener;
     this.shutdownState = shutdownState;
     this.eventsTopicName = eventsTopicName;
+    this.groupId = groupId;
   }
 
   @Override
@@ -59,7 +68,21 @@ public class EventsBrokerMessageConsumer implements Consumer<Event>, LifecycleLi
 
   @Override
   public void start() {
-    eventsBroker.get().receiveAsync(eventsTopicName, this);
+    BrokerApi brokerApi = eventsBroker.get();
+    boolean isGroupIdDefined = !Strings.isNullOrEmpty(groupId);
+    if (brokerApi instanceof ExtendedBrokerApi) {
+      if (brokerApi instanceof ExtendedBrokerApi && isGroupIdDefined) {
+        ((ExtendedBrokerApi) brokerApi).receiveAsync(eventsTopicName, groupId, this);
+        return;
+      }
+
+      if (isGroupIdDefined) {
+        logger.atWarning().log(
+            "Consumer has NOT been configured with groupId: %s because brokerApi is not bound to ExtendedBrokerApi concrete implementation",
+            groupId);
+      }
+      brokerApi.receiveAsync(eventsTopicName, this);
+    }
   }
 
   @Override
