870a6f53da93984f684f706e17d0ab53d9ea4279
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-09-11 17:17:28 +0100

    Allow subscribing to the topic with custom group id
    Previously, when the pull-replication plugin was set up with events-*
    plugins (such as events-kafka, events-aws-kinesis,events-gcloud-pubsub),
    consumer configurations depended on the groupId specified in the
    events-* plugin configuration. If no groupId was provided, it defaulted
    to using the value "gerrit.instance-id."
    
    Now, thanks to this new feature, the pull-replication plugin offers
    the option to define a group id when the property
    "replication.eventBrokerGroupId" is specified in the replication.conf
    file. Additionally, this feature is applicable when the events-* plugin
    implements the ExtendedBrokerApi interface.
    
    Bug: Issue 299327285
    Change-Id: I33d056ebd3e80b6a4c9d8eb02a0c2c1b591e8eb5

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/PullReplicationModule.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/PullReplicationModule.java
@@ -152,7 +152,10 @@ class PullReplicationModule extends AbstractModule {
     if (replicationConfig.getBoolean("replication", "consumeStreamEvents", false)) {
       install(new StreamEventModule());
     } else if (eventBrokerTopic != null) {
-      install(new EventsBrokerConsumerModule(eventBrokerTopic));
+      install(
+          new EventsBrokerConsumerModule(
+              eventBrokerTopic,
+              replicationConfig.getString("replication", null, "eventBrokerGroupId")));
     }
 
     DynamicSet.setOf(binder(), ReplicationStateListener.class);
