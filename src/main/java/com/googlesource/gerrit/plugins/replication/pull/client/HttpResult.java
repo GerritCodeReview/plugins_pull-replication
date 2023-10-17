228ab7c4cbd6cc18fefd7c336e97d8051b67d417
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-10-17 12:25:17 +0100

    Extend FetchApiClient interface to init project with config.
    This change extends the FetchApiClient interface by adding the
    ability to replicate the "init project" operation along with
    its configuration.
    
    The new feature involves sending an HTTP PUT request to the
    endpoint: pull-replication/init-project-config/<project_name>.git,
    including the configuration (refs/meta/config) as part of the
    request body. To maintain backward compatibility with Gerrit instances
    running older versions that lack the mentioned endpoint, a fallback mechanism is implemented that sends a request to the previous endpoint.
    
    It's important to note that this modification has no impact on
    the critical path, and therefore, there is no risk of disrupting
    any functionality.
    
    Bug: Issue 296854545
    Change-Id: I01fc49c5327b48efc7f3292c313a067076b4b0ed

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/client/HttpResult.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/client/HttpResult.java
@@ -15,6 +15,7 @@
 package com.googlesource.gerrit.plugins.replication.pull.client;
 
 import static javax.servlet.http.HttpServletResponse.SC_CONFLICT;
+import static javax.servlet.http.HttpServletResponse.SC_METHOD_NOT_ALLOWED;
 
 import com.google.gerrit.entities.Project;
 import java.util.Optional;
@@ -43,6 +44,10 @@ public class HttpResult {
 
   public boolean isParentObjectMissing() {
     return responseCode == SC_CONFLICT;
+  }
+
+  public boolean isNotAllowed() {
+    return responseCode == SC_METHOD_NOT_ALLOWED;
   }
 
   @Override
