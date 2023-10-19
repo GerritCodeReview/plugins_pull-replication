c4965bc579c33c506615aba01cbba002202d8696
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-10-19 17:39:45 +0100

    Revise the "init project" endpoint to manage project configuration
    This change expands the functionalities of the Java class
    "ProjectInitializationAction" to process the payload when it is
    present. When the payload is available, the logic will be as follows:
    
    - Create the project within the file system.
    - Parse the payload into a RevisionsInput object.
    - Validate the RevisionsInput object.
    - Execute the apply objects operation on the RevisionsInput object.
    - Cache the project.
    
    It's essential to emphasize that this modification will have no impact
    on earlier Gerrit instances that use older versions of the
    pull-replication plugin when replicating "init project" without
    including the project configuration in the payload.
    
    Bug: Issue 296854545
    Change-Id: Ib737003754f68db7484f3b0fc7a7c9e12e4073f5

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/api/ProjectInitializationAction.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/api/ProjectInitializationAction.java
@@ -14,25 +14,34 @@
 
 package com.googlesource.gerrit.plugins.replication.pull.api;
 
+import static com.googlesource.gerrit.plugins.replication.pull.PullReplicationLogger.repLog;
 import static com.googlesource.gerrit.plugins.replication.pull.api.HttpServletOps.checkAcceptHeader;
 import static com.googlesource.gerrit.plugins.replication.pull.api.HttpServletOps.setResponse;
 
+import com.google.common.base.Strings;
 import com.google.common.flogger.FluentLogger;
 import com.google.gerrit.entities.Project;
 import com.google.gerrit.entities.RefNames;
 import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.Url;
 import com.google.gerrit.index.project.ProjectIndexer;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.permissions.GlobalPermission;
 import com.google.gerrit.server.permissions.PermissionBackend;
 import com.google.gerrit.server.permissions.PermissionBackendException;
+import com.google.gerrit.server.project.ProjectCache;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
 import com.googlesource.gerrit.plugins.replication.LocalFS;
 import com.googlesource.gerrit.plugins.replication.pull.GerritConfigOps;
+import com.googlesource.gerrit.plugins.replication.pull.api.data.RevisionsInput;
+import com.googlesource.gerrit.plugins.replication.pull.api.exception.MissingParentObjectException;
+import com.googlesource.gerrit.plugins.replication.pull.api.exception.RefUpdateException;
+import com.googlesource.gerrit.plugins.replication.pull.api.util.PayloadSerDes;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Optional;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -51,17 +60,23 @@ public class ProjectInitializationAction extends HttpServlet {
   private final Provider<CurrentUser> userProvider;
   private final PermissionBackend permissionBackend;
   private final ProjectIndexer projectIndexer;
+  private final ApplyObjectCommand applyObjectCommand;
+  private final ProjectCache projectCache;
 
   @Inject
   ProjectInitializationAction(
       GerritConfigOps gerritConfigOps,
       Provider<CurrentUser> userProvider,
       PermissionBackend permissionBackend,
-      ProjectIndexer projectIndexer) {
+      ProjectIndexer projectIndexer,
+      ApplyObjectCommand applyObjectCommand,
+      ProjectCache projectCache) {
     this.gerritConfigOps = gerritConfigOps;
     this.userProvider = userProvider;
     this.permissionBackend = permissionBackend;
     this.projectIndexer = projectIndexer;
+    this.applyObjectCommand = applyObjectCommand;
+    this.projectCache = projectCache;
   }
 
   @Override
@@ -74,46 +89,124 @@ public class ProjectInitializationAction extends HttpServlet {
     }
 
     String path = httpServletRequest.getRequestURI();
-    String projectName = Url.decode(path.substring(path.lastIndexOf('/') + 1));
+    String gitRepositoryName = Url.decode(path.substring(path.lastIndexOf('/') + 1));
+    String projectName = gitRepositoryName.replace(".git", "");
     try {
-      if (initProject(projectName)) {
+      boolean initProjectStatus =
+          (httpServletRequest.getContentLength() == 0)
+              ? processRequest(gitRepositoryName)
+              : processRequestWithConfiguration(httpServletRequest, gitRepositoryName, projectName);
+
+      if (initProjectStatus) {
         setResponse(
             httpServletResponse,
             HttpServletResponse.SC_CREATED,
             "Project " + projectName + " initialized");
         return;
       }
-    } catch (AuthException | PermissionBackendException e) {
+
       setResponse(
           httpServletResponse,
-          HttpServletResponse.SC_FORBIDDEN,
-          "User not authorized to create project " + projectName);
-      return;
+          HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+          "Cannot initialize project " + projectName);
+    } catch (Exception e) {
+      handleException(httpServletResponse, e, projectName);
     }
-
-    setResponse(
-        httpServletResponse,
-        HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-        "Cannot initialize project " + projectName);
   }
 
-  public boolean initProject(String projectName) throws AuthException, PermissionBackendException {
+  public boolean initProject(String gitRepositoryName)
+      throws AuthException, PermissionBackendException {
+    return initProject(gitRepositoryName, true);
+  }
+
+  private boolean processRequest(String gitRepositoryName)
+      throws AuthException, PermissionBackendException {
+    if (initProject(gitRepositoryName)) {
+      repLog.info("Init project API from {}", gitRepositoryName);
+      return true;
+    }
+    return false;
+  }
+
+  private boolean processRequestWithConfiguration(
+      HttpServletRequest httpServletRequest, String gitRepositoryName, String projectName)
+      throws AuthException, PermissionBackendException, IOException, BadRequestException,
+          MissingParentObjectException, RefUpdateException {
+    if (!initProject(gitRepositoryName, false)) {
+      return false;
+    }
+
+    RevisionsInput input = PayloadSerDes.parseRevisionsInput(httpServletRequest);
+    validateInput(input);
+    applyObjectCommand.applyObjects(
+        Project.nameKey(projectName),
+        input.getRefName(),
+        input.getRevisionsData(),
+        input.getLabel(),
+        input.getEventCreatedOn());
+    projectCache.onCreateProject(Project.nameKey(projectName));
+    repLog.info(
+        "Init project API from {} for {}:{} - {}",
+        input.getLabel(),
+        projectName,
+        input.getRefName(),
+        Arrays.toString(input.getRevisionsData()));
+    return true;
+  }
+
+  private boolean initProject(String gitRepositoryName, boolean needsProjectReindexing)
+      throws AuthException, PermissionBackendException {
     // When triggered internally(for example by consuming stream events) user is not provided
     // and internal user is returned. Project creation should be always allowed for internal user.
     if (!userProvider.get().isInternalUser()) {
       permissionBackend.user(userProvider.get()).check(GlobalPermission.CREATE_PROJECT);
     }
-    Optional<URIish> maybeUri = gerritConfigOps.getGitRepositoryURI(projectName);
+    Optional<URIish> maybeUri = gerritConfigOps.getGitRepositoryURI(gitRepositoryName);
     if (!maybeUri.isPresent()) {
-      logger.atSevere().log("Cannot initialize project '%s'", projectName);
+      logger.atSevere().log("Cannot initialize project '%s'", gitRepositoryName);
       return false;
     }
     LocalFS localFS = new LocalFS(maybeUri.get());
-    Project.NameKey projectNameKey = Project.NameKey.parse(projectName);
+    Project.NameKey projectNameKey = Project.NameKey.parse(gitRepositoryName);
     if (localFS.createProject(projectNameKey, RefNames.HEAD)) {
-      projectIndexer.index(projectNameKey);
+      if (needsProjectReindexing) {
+        projectIndexer.index(projectNameKey);
+      }
       return true;
     }
     return false;
+  }
+
+  private void validateInput(RevisionsInput input) {
+    if (Strings.isNullOrEmpty(input.getLabel())) {
+      throw new IllegalArgumentException("Source label cannot be null or empty");
+    }
+
+    if (Strings.isNullOrEmpty(input.getRefName())) {
+      throw new IllegalArgumentException("Ref-update refname cannot be null or empty");
+    }
+    input.validate();
+  }
+
+  private void handleException(HttpServletResponse response, Exception e, String projectName)
+      throws IOException {
+    repLog.error("Init Project API FAILED from {}", projectName);
+    logger.atSevere().withCause(e).log("Exception while creating project %s", projectName);
+
+    int statusCode = HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+    String errorMessage = "Internal Server Error";
+
+    if (e instanceof BadRequestException || e instanceof IllegalArgumentException) {
+      statusCode = HttpServletResponse.SC_BAD_REQUEST;
+      errorMessage = "Invalid request payload";
+    } else if (e instanceof RefUpdateException || e instanceof MissingParentObjectException) {
+      statusCode = HttpServletResponse.SC_CONFLICT;
+      errorMessage = e.getMessage();
+    } else if (e instanceof AuthException || e instanceof PermissionBackendException) {
+      statusCode = HttpServletResponse.SC_FORBIDDEN;
+      errorMessage = "User not authorized to create project " + projectName;
+    }
+
+    setResponse(response, statusCode, errorMessage);
   }
 }
