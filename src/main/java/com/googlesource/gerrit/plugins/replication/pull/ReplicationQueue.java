3f893fe51cd34a932fd754e8c7e683fdfda640ca
Marcin Czech <maczech@gmail.com>
2023-10-25 13:13:21 +0200

    Improve log message when event is fired
    When pull-replication is not fully loaded, events are stored in
    the memory queue. This fact is logged in the `pull_replication_log`
    file. For better readability, add all information about the stored
    event.
    
    Bug: Issue 307574381
    Change-Id: Ife992fefdf1a8bb880ffcc69aa0ab14ed08afe0b

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/ReplicationQueue.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/ReplicationQueue.java
@@ -235,7 +235,10 @@ public class ReplicationQueue
       ReplicationState state) {
     if (!running) {
       stateLog.warn(
-          "Replication plugin did not finish startup before event, event replication is postponed",
+          String.format(
+              "Replication plugin did not finish startup before event, event replication is postponed"
+                  + " for event %s",
+              event),
           state);
       beforeStartupEventsQueue.add(
           ReferenceUpdatedEvent.create(project.get(), refName, objectId, eventCreatedOn, isDelete));
