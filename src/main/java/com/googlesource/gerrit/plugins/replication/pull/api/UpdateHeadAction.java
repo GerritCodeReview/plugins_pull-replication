1ab15a377f8bd7d3ee8a0e236a7384c0d0e66c61
Dariusz Luksza <dariusz.luksza@gmail.com>
2023-12-01 16:35:11 +0000

    Update project HEAD from stream event
    Handle `ProjectHeadUpdatedEvent` and update project's `HEAD` reference
    on the replica.
    
    Bug: Issue 40014694
    Change-Id: Ib9a83442c390a493eb839d9edb115148d6f07438

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/api/UpdateHeadAction.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/api/UpdateHeadAction.java
@@ -15,49 +15,26 @@
 package com.googlesource.gerrit.plugins.replication.pull.api;
 
 import com.google.common.base.Strings;
-import com.google.common.flogger.FluentLogger;
-import com.google.gerrit.entities.Project;
 import com.google.gerrit.entities.RefNames;
 import com.google.gerrit.extensions.api.projects.HeadInput;
-import com.google.gerrit.extensions.registration.DynamicItem;
 import com.google.gerrit.extensions.restapi.AuthException;
 import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.ResourceConflictException;
-import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
 import com.google.gerrit.extensions.restapi.Response;
 import com.google.gerrit.extensions.restapi.RestModifyView;
-import com.google.gerrit.extensions.restapi.UnprocessableEntityException;
-import com.google.gerrit.server.events.EventDispatcher;
-import com.google.gerrit.server.permissions.PermissionBackendException;
 import com.google.gerrit.server.project.ProjectResource;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import com.googlesource.gerrit.plugins.replication.LocalFS;
-import com.googlesource.gerrit.plugins.replication.pull.Context;
-import com.googlesource.gerrit.plugins.replication.pull.FetchRefReplicatedEvent;
-import com.googlesource.gerrit.plugins.replication.pull.GerritConfigOps;
-import com.googlesource.gerrit.plugins.replication.pull.ReplicationState;
-import java.net.HttpURLConnection;
-import java.util.Optional;
-import org.eclipse.jgit.lib.RefUpdate;
-import org.eclipse.jgit.transport.URIish;
 
 @Singleton
 public class UpdateHeadAction implements RestModifyView<ProjectResource, HeadInput> {
-  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
-  private final GerritConfigOps gerritConfigOps;
   private final FetchPreconditions preconditions;
-  private final DynamicItem<EventDispatcher> eventDispatcher;
-  private static final URIish EMPTY_URI = new URIish();
+  private final UpdateHeadCommand updateHeadCommand;
 
   @Inject
-  UpdateHeadAction(
-      GerritConfigOps gerritConfigOps,
-      FetchPreconditions preconditions,
-      DynamicItem<EventDispatcher> eventDispatcher) {
-    this.gerritConfigOps = gerritConfigOps;
+  UpdateHeadAction(FetchPreconditions preconditions, UpdateHeadCommand updateHeadCommand) {
     this.preconditions = preconditions;
-    this.eventDispatcher = eventDispatcher;
+    this.updateHeadCommand = updateHeadCommand;
   }
 
   @Override
@@ -74,50 +51,8 @@ public class UpdateHeadAction implements RestModifyView<ProjectResource, HeadInp
       throw new AuthException("Update head not permitted");
     }
 
-    // TODO: the .git suffix should not be added here, but rather it should be
-    //  dealt with by the caller, honouring the naming style from the
-    //  replication.config (Issue 15221)
-    Optional<URIish> maybeRepo =
-        gerritConfigOps.getGitRepositoryURI(String.format("%s.git", projectResource.getName()));
+    updateHeadCommand.doUpdate(projectResource.getNameKey(), ref);
 
-    try {
-      if (maybeRepo.isPresent()) {
-        if (new LocalFS(maybeRepo.get()).updateHead(projectResource.getNameKey(), ref)) {
-          return res = Response.ok(ref);
-        }
-        throw new UnprocessableEntityException(
-            String.format(
-                "Could not update HEAD of repo %s to ref %s", projectResource.getName(), ref));
-      }
-    } finally {
-      fireEvent(
-          projectResource.getNameKey(),
-          res != null && res.statusCode() == HttpURLConnection.HTTP_OK);
-    }
-    throw new ResourceNotFoundException(
-        String.format("Could not compute URL for repo: %s", projectResource.getName()));
-  }
-
-  private void fireEvent(Project.NameKey projectName, boolean succeeded) {
-    try {
-      Context.setLocalEvent(true);
-      eventDispatcher
-          .get()
-          .postEvent(
-              new FetchRefReplicatedEvent(
-                  projectName.get(),
-                  RefNames.HEAD,
-                  EMPTY_URI, // TODO: the remote label is not passed as parameter, hence cannot be
-                  // propagated to the event
-                  succeeded
-                      ? ReplicationState.RefFetchResult.SUCCEEDED
-                      : ReplicationState.RefFetchResult.FAILED,
-                  RefUpdate.Result.FORCED));
-    } catch (PermissionBackendException e) {
-      logger.atSevere().withCause(e).log(
-          "Cannot post event for refs/meta/config on project %s", projectName);
-    } finally {
-      Context.unsetLocalEvent();
-    }
+    return Response.ok(ref);
   }
 }
