976232d7b7f283f3a8aba9b9c09c7686275cd5ea
Diego Zambelli Sessona <diego.sessona@gmail.com>
2023-12-05 15:35:40 +0100

    Remove events from beforeStartupEventsQueue after fire()
    The plugin was removing the event before firing it from the
    beforeStartupEventsQueue. This could have caused a message
    loss in case the configuration was changed before firing the event.
    
    Now after the plugin is fully loaded each event is
    removed from the the in-memory queue after being fired.
    
    Follow up of change 391054.
    
    Change-Id: I4ccef01f42dc11c5df84ca8dfa231e6e895d8a2d

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/ReplicationQueue.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/ReplicationQueue.java
@@ -549,7 +549,7 @@ public class ReplicationQueue
   private void fireBeforeStartupEvents() {
     Set<String> eventsReplayed = new HashSet<>();
     ReferenceUpdatedEvent event;
-    while ((event = beforeStartupEventsQueue.poll()) != null) {
+    while ((event = beforeStartupEventsQueue.peek()) != null) {
       String eventKey = String.format("%s:%s", event.projectName(), event.refName());
       if (!eventsReplayed.contains(eventKey)) {
         repLog.info("Firing pending task {}", event);
@@ -561,6 +561,7 @@ public class ReplicationQueue
             event.isDelete());
         eventsReplayed.add(eventKey);
       }
+      beforeStartupEventsQueue.remove(event);
     }
   }
 
