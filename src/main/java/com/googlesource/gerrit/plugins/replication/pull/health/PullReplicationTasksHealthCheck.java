b95b4cd0f8a5a5d9a2f25571d95561238363f11b
Christoforos Miliotis <christoforos.miliotis.dev@gmail.com>
2024-01-22 10:20:06 +0000

    Read heathcheck settings from plugin configuration
    Some settings are specific to the external healthcheck; the list of
    repos and the periodOfTime. These settings are defined in the plugin's
    configuration file.
    
    Read custom settings from the plugin's config file. "Core" settings
    like the `enabled` flag and the timeout are still defined in the
    healthcheck plugin's config file.
    
    Bug: Issue 312895374
    Change-Id: I8454b19770e4cba986b1e33bddf15f00bc7a8f70

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/health/PullReplicationTasksHealthCheck.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/health/PullReplicationTasksHealthCheck.java
@@ -14,24 +14,58 @@
 
 package com.googlesource.gerrit.plugins.replication.pull.health;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.metrics.MetricMaker;
+import com.google.gerrit.server.config.ConfigUtil;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.googlesource.gerrit.plugins.healthcheck.HealthCheckConfig;
 import com.googlesource.gerrit.plugins.healthcheck.check.AbstractHealthCheck;
+import com.googlesource.gerrit.plugins.replication.ConfigResource;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import org.eclipse.jgit.lib.Config;
 
 @Singleton
 public class PullReplicationTasksHealthCheck extends AbstractHealthCheck {
+  private static final long DEFAULT_PERIOD_OF_TIME_SECS = 10L;
+  public static final String HEALTHCHECK_NAME_SUFFIX = "-outstanding-tasks";
+  private final Set<String> projects;
+  private final long periodOfTimeSec;
 
   @Inject
   public PullReplicationTasksHealthCheck(
       ListeningExecutorService executor,
-      HealthCheckConfig config,
+      HealthCheckConfig healthCheckConfig,
+      ConfigResource configResource,
       @PluginName String name,
       MetricMaker metricMaker) {
-    super(executor, config, name + "-tasks", metricMaker);
+    super(executor, healthCheckConfig, name + HEALTHCHECK_NAME_SUFFIX, metricMaker);
+    String healthCheckName = name + HEALTHCHECK_NAME_SUFFIX;
+
+    Config replicationConfig = configResource.getConfig();
+    this.projects =
+        Set.of(
+            replicationConfig.getStringList(
+                HealthCheckConfig.HEALTHCHECK, healthCheckName, "projects"));
+    this.periodOfTimeSec =
+        ConfigUtil.getTimeUnit(
+            replicationConfig,
+            HealthCheckConfig.HEALTHCHECK,
+            healthCheckName,
+            "periodOfTime",
+            DEFAULT_PERIOD_OF_TIME_SECS,
+            TimeUnit.SECONDS);
+  }
+
+  public long getPeriodOfTimeSec() {
+    return periodOfTimeSec;
+  }
+
+  public Set<String> getProjects() {
+    return ImmutableSet.copyOf(projects);
   }
 
   @Override
---
+++ src/main/resources/Documentation/healthcheck.md
@@ -0,0 +1,34 @@
+@PLUGIN@ health checks
+==============
+
+The @PLUGIN@ plugin registers the `pull-replication-outstanding-tasks`
+healthcheck.
+
+Health check configuration
+--------------------------
+
+The configuration of the health check is split across two files.
+- The "standard" properties commonly available to all other checks
+of the `healthcheck` plugin. These are set in the `healthcheck` plugin's
+[config file](https://gerrit.googlesource.com/plugins/healthcheck/+/refs/heads/master/src/main/resources/Documentation/config.md#settings).
+- Settings specific to the check are set in the plugin's [config file](./config.md#file-pluginconfig).
+
+
+The health check can be configured as follows:
+- `healthcheck.@PLUGIN@-outstanding-tasks.projects`: The repo(s) that
+the health check will track pending replication tasks against. Multiple
+entries are supported. This is a mandatory setting.
+- `healthcheck.@PLUGIN@-outstanding-tasks.periodOfTime`: The time for
+which the check needs to be successful, in order for the instance to be
+marked healthy. If the time unit is omitted it defaults to seconds.
+
+Useful information
+------------------
+
+- The health check is registered only when the [healthcheck](https://gerrit.googlesource.com/plugins/healthcheck) plugin
+is installed. If the `healthcheck` plugin is not installed, then the
+check registration is skipped during load of the pull-replication
+plugin.
+- The caveats of external health checks are applicable. Please read the
+`Gotchas And Limitations` of the `Gerrit-ApiModule` section in the [docs](https://gerrit.googlesource.com/gerrit/+/refs/heads/master/Documentation/dev-plugins.txt).
+
