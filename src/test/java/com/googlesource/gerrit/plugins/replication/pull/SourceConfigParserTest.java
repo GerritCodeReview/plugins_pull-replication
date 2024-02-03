78de6c2b87e2a9ab50880ef7ec413ebfccf3ea69
Jacek Centkowski <geminica.programs@gmail.com>
2024-02-03 16:04:37 +0100

    Introduce the `remote.NAME.fetchEvery` configuration param
    Currently, there are three methods for obtaining repository updates in
    pull-replication:
    * Apply objects - This transfers updated refs and objects via the REST
      API; optimisation on the top of Git fetch for small payloads and for
      subset of refs
    * Trigger git fetch (as configured preference) or fallback in all cases
      when apply object is not suitable or fails
    * Event tiggered through the event-broker.
    
    The parameter (if configured and when implemented) enables the fourth,
    time-based (every `n` seconds) method, that is independent of the
    REST API. It will periodically invoke git fetch to detect and retrieve
    new data as it becomes available. Note that it is meant for the remote
    that doesn't offer any events or webhooks to provide information about
    new data.
    
    The default parameter value is `0s` which means that periodical fetch
    is disabled. Note that larger time units (`m`, `h`, etc...) can be
    used to configure it conveniently.
    
    Note that enbaling periodic fetch and REST API (IOW having both `apiUrl`
    and `fetchEvery` configured) may lead to racy writes to the repository
    and as such is considered an invalid configuration. Such configuration
    results in reported as error and prevents plugin from start.
    
    Bug: Issue 322146240
    Change-Id: I23fc5e23aabff2dd0f053de235245614cf4706c9

---
+++ src/test/java/com/googlesource/gerrit/plugins/replication/pull/SourceConfigParserTest.java
@@ -0,0 +1,57 @@
+// Copyright (C) 2024 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replication.pull;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.testing.GerritJUnit.assertThrows;
+
+import com.google.inject.util.Providers;
+import com.googlesource.gerrit.plugins.replication.ReplicationConfig;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.Config;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SourceConfigParserTest {
+  @Mock ReplicationConfig replicationConfigMock;
+
+  private SourceConfigParser objectUnderTest;
+
+  @Before
+  public void setup() {
+    objectUnderTest = new SourceConfigParser(false, Providers.of(replicationConfigMock));
+  }
+
+  @Test
+  public void shouldThrowWhenBothFetchEveryAndApiUrlsConfigured() {
+    // given
+    Config config = new Config();
+    config.setString("remote", "test_remote", "fetch", "+refs/*:refs/*");
+    config.setString("remote", "test_remote", "projects", "to_be_replicated");
+
+    config.setString("remote", "test_remote", "apiUrl", "http://foo.bar/api");
+    config.setString("remote", "test_remote", "fetchEvery", "1s");
+
+    // when/then
+    String errorMessage =
+        assertThrows(ConfigInvalidException.class, () -> objectUnderTest.parseRemotes(config))
+            .getMessage();
+    assertThat(errorMessage).contains("invalid configuration");
+  }
+}
