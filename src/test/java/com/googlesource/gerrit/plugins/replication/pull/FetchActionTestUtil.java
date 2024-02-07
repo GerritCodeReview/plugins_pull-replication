079bd7c3c44dd7d9321e7a99fdf46db1d83034a6
Antonio Barone <syntonyze@gmail.com>
2024-02-07 16:07:36 +0100

    Improve payload creation for batch-fetch requests
    Both fetch API client and test coverage make extensive usage of JSON
    crafting by hand. This makes the code error prone as well as tests
    difficult to read.
    
    Start making a step forward in tidying up this pattern by addressing
    specifically the usage and the testing of batch-fetch API payloads,
    which came up during the review of I0c8591ee.
    
    Change-Id: I0ccc9a05d429d7db5fffd748b31a765927d36c5b

---
+++ src/test/java/com/googlesource/gerrit/plugins/replication/pull/FetchActionTestUtil.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.googlesource.gerrit.plugins.replication.pull;
+
+import com.google.gson.Gson;
+import com.googlesource.gerrit.plugins.replication.pull.api.FetchAction;
+import com.googlesource.gerrit.plugins.replication.pull.api.FetchAction.RefInput;
+import com.googlesource.gerrit.plugins.replication.pull.api.HttpPayloadGsonProvider;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class FetchActionTestUtil {
+
+  private static final Gson gson = HttpPayloadGsonProvider.get();
+
+  public static FetchAction.BatchInput createBatchInput(
+      String label, boolean async, RefInput... refInputs) {
+    FetchAction.BatchInput batchInput = new FetchAction.BatchInput();
+    batchInput.label = label;
+    batchInput.async = async;
+    batchInput.refInputs = Arrays.stream(refInputs).collect(Collectors.toSet());
+    return batchInput;
+  }
+
+  public static FetchAction.BatchInput createBatchInput(
+      String label, boolean async, String... refNames) {
+    return createBatchInput(
+        label, false, Arrays.stream(refNames).map(RefInput::create).toArray(RefInput[]::new));
+  }
+
+  public static <T> String toJsonString(T obj) {
+    return gson.toJson(obj);
+  }
+}
