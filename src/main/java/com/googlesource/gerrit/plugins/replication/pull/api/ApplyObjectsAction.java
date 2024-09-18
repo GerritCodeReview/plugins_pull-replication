0257141aa6293862ecdc4ded7c84518178be5168
Luca Milanesio <luca.milanesio@gmail.com>
2024-09-18 15:28:52 +0100

    Ban synchronous ref-delete via apply-object(s)
    Ref deletion is a non-fast-forward operation and it may
    have different outcome with other forced operations when
    not executed in the correct order.
    
    Ban the use of ref-delete via apply-object returning a 400
    and force the client to run an asynchronous fetch instead
    which would also go through global-refdb validation.
    For example, a forced update and a delete cannto be swapped.
    
    Also remove the now unused async DeleteRefJob because the
    deletion is managed in the same replication queue as any
    other ref.
    
    Bug: Issue 367651566
    Change-Id: I94d2850c4aad7573fabfd83bd361e1e683b8cb19

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/api/ApplyObjectsAction.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/api/ApplyObjectsAction.java
@@ -34,21 +34,15 @@ import com.googlesource.gerrit.plugins.replication.pull.api.exception.RefUpdateE
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Objects;
-import javax.servlet.http.HttpServletResponse;
 
 public class ApplyObjectsAction implements RestModifyView<ProjectResource, RevisionsInput> {
 
   private final ApplyObjectCommand command;
-  private final DeleteRefCommand deleteRefCommand;
   private final FetchPreconditions preConditions;
 
   @Inject
-  public ApplyObjectsAction(
-      ApplyObjectCommand command,
-      DeleteRefCommand deleteRefCommand,
-      FetchPreconditions preConditions) {
+  public ApplyObjectsAction(ApplyObjectCommand command, FetchPreconditions preConditions) {
     this.command = command;
-    this.deleteRefCommand = deleteRefCommand;
     this.preConditions = preConditions;
   }
 
@@ -65,6 +59,14 @@ public class ApplyObjectsAction implements RestModifyView<ProjectResource, Revis
       if (Strings.isNullOrEmpty(input.getRefName())) {
         throw new BadRequestException("Ref-update refname cannot be null or empty");
       }
+      if (Objects.isNull(input.getRevisionsData())) {
+        repLog.error(
+            "Apply object API *FAILED* from {} for {}:{} - revision data is null",
+            input.getLabel(),
+            resource.getNameKey(),
+            input.getRefName());
+        throw new BadRequestException("Null revision data");
+      }
 
       repLog.info(
           "Apply object API from {} for {}:{} - {}",
@@ -72,16 +74,6 @@ public class ApplyObjectsAction implements RestModifyView<ProjectResource, Revis
           resource.getNameKey(),
           input.getRefName(),
           Arrays.toString(input.getRevisionsData()));
-
-      if (Objects.isNull(input.getRevisionsData())) {
-        deleteRefCommand.deleteRef(resource.getNameKey(), input.getRefName(), input.getLabel());
-        repLog.info(
-            "Apply object API - REF DELETED - from {} for {}:{}",
-            input.getLabel(),
-            resource.getNameKey(),
-            input.getRefName());
-        return Response.withStatusCode(HttpServletResponse.SC_NO_CONTENT, "");
-      }
 
       try {
         input.validate();
