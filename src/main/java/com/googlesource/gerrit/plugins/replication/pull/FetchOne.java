6286e38fea8b2e0a0df158e787ba711d451a5f8b
Luca Milanesio <luca.milanesio@gmail.com>
2025-02-20 06:29:37 +0000

    Fix the create/delete of ref-specs in FetchOne delta
    When adding a ref-update to a ref-delete on FetchOne or
    ref-delete to an existing ref-update, the existing operation
    should be removed and only the last one should take effect.
    
    Change-Id: I1d2a3a492db0a1b45bfbdf0ce89030656ebbb338

--- src/main/java/com/googlesource/gerrit/plugins/replication/pull/FetchOne.java
+++ src/main/java/com/googlesource/gerrit/plugins/replication/pull/FetchOne.java
@@ -214,6 +214,21 @@ public class FetchOne implements ProjectRunnable, CanceledWhileRunning, Completa
       fetchAllRefs = true;
       repLog.trace("[{}] Added all refs for replication from {}", taskIdHex, uri);
     } else if (!fetchAllRefs) {
+      // The ref-spec could be already present as either
+      // update or delete, therefore a simple addition to the
+      // set may risk to have both update and delete for the same
+      // ref, which would be an issue: the delta is an unordered set
+      // and executing updates and deletes for the same ref may have
+      // unexpected results, depending on which one you execute first.
+
+      // Delete any previous ref-spec as delete
+      delta.remove(FetchRefSpec.fromRef(":" + ref.refName()));
+
+      // Delete any previous ref-spec as update
+      delta.remove(FetchRefSpec.fromRef(ref.refName()));
+
+      // Add the new ref as-is for making sure that
+      // there is only one operation per refName.
       delta.add(ref);
       repLog.trace("[{}] Added ref {} for replication from {}", taskIdHex, ref, uri);
     }
