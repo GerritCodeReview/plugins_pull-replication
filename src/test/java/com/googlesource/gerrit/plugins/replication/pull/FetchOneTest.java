d0ca3d505f1d4f8eaed7557dfe12a42d02d7bc84
Luca Milanesio <luca.milanesio@gmail.com>
2026-05-24 11:51:20 +0100

    Introduce tests for ReplicationTaskId thread-local
    Ensure that the ReplicationTaskId introduced in Iacacda97 preserves
    nesting of task-ids with or without exceptions.
    
    Also assures that the withTaskId() is used in conjunction with refs
    filtering.
    
    Change-Id: Id2af833f94b68bde20124cca695524a254b44790

--- src/test/java/com/googlesource/gerrit/plugins/replication/pull/FetchOneTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/replication/pull/FetchOneTest.java
@@ -46,6 +46,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -366,6 +367,29 @@ public class FetchOneTest {
             urIish,
             ReplicationState.RefFetchResult.FAILED,
             null);
+  }
+
+  @Test
+  public void shouldSetReplicationTaskIdDuringRefsFiltering() throws Exception {
+    AtomicReference<String> replicationTaskIdDuringFiltering = new AtomicReference<>();
+    assertThat(ReplicationTaskId.get()).isNull();
+
+    setupObjectUnderTestMocksForFetchingTestRef();
+
+    objectUnderTest.setReplicationFetchFilter(replicationFilter);
+    when(replicationFilter.get())
+        .thenReturn(
+            (projectName, fetchRefs) -> {
+              replicationTaskIdDuringFiltering.set(ReplicationTaskId.get());
+              return Set.of(TEST_REF);
+            });
+
+    objectUnderTest.run();
+
+    assertThat(ReplicationTaskId.get()).isNull();
+    String taskIdDuringFiltering = replicationTaskIdDuringFiltering.get();
+    assertThat(taskIdDuringFiltering).isNotEmpty();
+    assertThat(taskIdDuringFiltering).isNotNull();
   }
 
   @Test
@@ -888,6 +912,15 @@ public class FetchOneTest {
   public void shouldDeleteWhenDeleteRef() {
     List<FetchRefSpec> fetchRefSpecs = List.of(FetchRefSpec.fromRef(":refs/something/someref"));
     assertThat(refsToDelete(fetchRefSpecs)).isEqualTo(Set.of("refs/something/someref"));
+  }
+
+  private void setupObjectUnderTestMocksForFetchingTestRef() throws Exception {
+    setupMocks(true);
+    Set<FetchRefSpec> refSpecs = Set.of(TEST_REF_SPEC);
+    setupFetchFactoryMock(
+        List.of(new FetchFactoryEntry.Builder().refSpecNameWithDefaults(TEST_REF).build()),
+        Optional.of(List.of(TEST_REF)));
+    objectUnderTest.addRefs(refSpecs);
   }
 
   private void setupRequestScopeMock() {
